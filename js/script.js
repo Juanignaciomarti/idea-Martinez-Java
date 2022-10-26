908c52ee827cffaa47094f9a03769dcd35107180
Juan Ignacio Martinez <juanignaciomartinez3@gmail.com>
2022-10-25 22:14:14 -0300

    3

--- js/script.js
+++ js/script.js
@@ -116,25 +116,6 @@ log_out_btn.onclick = () => {
 }
 
 
-
-/*class Tarjeta {
-    constructor(imagen, texto ,opcion1, opcion2) {
-        this.imagen = imagen
-        this.texto = texto
-        this.opcion1 = opcion1
-        this.opcion2 = opcion2
-    }
-}
-
-
-/*const tarjeta = [{
-    texto: "Fede siempre resaltò por ser el mas curioso, el mas aventurero y el mas osado , si se quiere, del grupo",
-    img: "./img/1.webp"
-}
-]
-*/
-
-
 book.innerHTML = `
 <p> Fede siempre resaltò por ser el mas curioso, el mas aventurero y el mas osado , si se quiere, del grupo</p>
 <p>No era extraño, que desapareciera por dias...debe estar en alguna de sus tantas aventuras, deciamos</p>
@@ -146,11 +127,6 @@ book.innerHTML = `
 
 /*FIN PRE ENTREGA Nº3* ----------------------------------------------------*/
 
-/*class Tarjeta {
-    constructor(texto, opcion1, opcion2) {
-            // completar
-    }
-}/*
 /*class personaje{
     constructor(nombre, edad, sexo, altura, peso){
         this.nombre = nombre.toUpperCase();
@@ -174,21 +150,7 @@ book.innerHTML = `
     }
     
         }
-    
-}
 
-class tarjeta {
-    constructor(texto, boton1, boton2, continuar){
-        this.texto = texto
-        this.boton1 = boton1
-        this.boton2 = boton2
-        this.continuar = continuar 
-    }
-    
-
-}
-
-const tarjeta1 = new tarjeta ()
 
 
 /*alert("Bueno, antes de comenzar, voy a necesitar tus datos")*/
@@ -306,3 +268,29 @@ console.log(mochila)
 const mochilaUnificada = mochila1.concat(mochila);
 console.log(mochilaUnificada);*/
 
+const textoTarjetas = [
+    "Fede siempre resaltò por ser el mas curioso, el mas aventurero y el mas osado , si se quiere, del grupo",
+    "No era extraño, que desapareciera por dias...debe estar en alguna de sus tantas aventuras, deciamos",
+    "Por eso, cuando viajo a Japòn no le dimos demasiada importancia...",
+    "'-Quiero conocer distintos lugares, Fukushima y Aokigahara.'",
+    "'-Estas seguro Fede? ese lugar debe seguir con niveles de radiaciòn altos..' dije. No le di mucha importancia a Aokigahara, estaba seguro que habia escuchado algo, pero en ese momento no lo recordaba.",
+    "Grave error, ahora estamos frente al bosque de Ahokigahara junto con un guia que no inspira demasiada confianza. Fede desaparecio exactamente hace 10 dias...y debemos encontrarlo",
+    "Bueno, antes de comenzar, voy a necesitar tus datos",
+    "Asi que " + nombrePersonaje + " Mucho gusto.",
+    "Antes de seguir, que llevas en la mochila?",
+    "El guìa nos comenta que tenemos 2 opciones, seguir por el camino preestablecido o saltar la valla perimetral. Conociendo a Fede, sabiamos que se habia adentrado en el bosque.",
+    "Pasamos el primer esocollo, sin saber que nos depara el bosque. La luz era decididamente escasa, y el comportamiento del guia parecia cada vez mas erratico",
+    "15 minutos despues, el guia se frena abruptamente, gira en nuestra direccion y dice 'no podemos seguir, tenemos que irnos'",
+    "'-imposible, no nos vamos a ir sin nuestro amigo.' dije. '- yo no voy a continuar, pero les voy dar un atajo...creo que se donde puede estar su amigo' ",
+    "caminen 10 pasos de donde estamos, veran que se abren 2 caminos doblen a la derecha. Yo los seguire de atras para asegurarme de que todo vaya bien",
+    "a medida que avanzavamos el bosque se hacia cada vez mas oscuro y silencioso. Me preocupaba perder la nocion del tiempo...",
+    "Te noto tenso "+ nombrePersonaje + " pero puedo entenderte, la oscuridad, el silencio...este lugar es una tumba...",
+    "mire mi celular para comprobar la hora y marcaba lo siguiente... ",
+    "Algo andaba mal, estaba seguro que habia configurado mi telefono correctamente. La hora no podia ser esa, ni el sitio geografico",
+    "Deberiamos doblar a la izquierda, y seguir ese camino...",
+    "Seguimos caminando, siguiendo un sendero precariamente trazado y que desaparecia levemente con cada paso que dabamos.",
+    "De repende, a un lado del camino, vi lo que parecia una mochila. Me acerque a inspeccionarla",
+    "contenia un encendedor, un cuchillo, varias ramas y un cuaderno",
+    "decidi tomar todos los elementos y guardarlos en mi mochila, que estaba vacia"
+
+]
